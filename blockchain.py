324ef5d9a3849791a88a12e49814b160c9db4476
Sina Qahramani <sinaqahramani@gmail.com>
2020-08-03 23:52:23 +0430

    register_node method moved down valid_chain method added completely

--- blockchain.py
+++ blockchain.py
@@ -16,15 +16,6 @@ class Blockchain():
         # Create the genesis block:
         self.new_block(previous_hash=1, proof=100)
 
-    def register_node(self, address):
-        """
-        Add a new node to the list of nodes
-        :param address: <str> Address of node. e.g.: http://192.168.0.5:5000'
-        :return: None
-        """
-        parsed_url = urlparse(address)
-        self.nodes.add(parsed_url.netloc)
-
     def new_block(self, proof, previous_hash=None):
         """
         Creates a new Block in the Blockchain
@@ -105,4 +96,40 @@ class Blockchain():
 
         return proof
 
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+        :param address: <str> Address of node. e.g.: http://192.168.0.5:5000'
+        :return: None
+        """
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def valid_chain(self, chain):
+        """
+        Determine if a given blockchain is valid
+        :param chain: <list> A blockchain
+        :return: <bool> True if valid, False if not
+        """
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+
+            # Check that the hash of the block is correct
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            # Check that the POW is correct
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
 
