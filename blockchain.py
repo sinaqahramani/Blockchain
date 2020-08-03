51d762d99d96e585cc32d4ef78163694813c87dd
Sina Qahramani <sinaqahramani@gmail.com>
2020-08-04 00:03:34 +0430

    resolve_conflicts method added completely

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import requests
 from time import time
 from urllib.parse import urlparse
 
@@ -132,4 +133,37 @@ class Blockchain():
             current_index += 1
 
         return True
+
+    def resolve_conflicts(self):
+        """
+        This is our Consensus Algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: <bool> True if our chain was replaced, False if not
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # We're only looking for chains longer than ours
+        max_length = len(self.chain)
+
+        # Grab an verify the chains from all the nodes in our network
+        for node in neighbours:
+            res = requests.get(f'http://{node}/chain')
+
+            if res.status_code == 200:
+                length = res.json()['length']
+                chain = res.json()['chain']
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # Replace our chain if we discovered a new, valid chain longer than ours
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
