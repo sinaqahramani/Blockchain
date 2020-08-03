a76462ebba0115db91a1c654f401ddbd468ddfa1
Sina Qahramani <sinaqahramani@gmail.com>
2020-08-03 22:14:57 +0430

    validated_proof staticmethod created pow method created

--- blockchain.py
+++ blockchain.py
@@ -69,3 +69,30 @@ class Blockchain():
         block_str = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_str).hexdigest()
 
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does Hash(last_proof,proof) contain 4 leading zeroses?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool>
+        """
+        g = f'{last_proof}{proof}'.encode()
+        g_hash = hashlib.sha256(g).hexdigest()
+        return g_hash[:4] == '0000'
+
+    def pow(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+
