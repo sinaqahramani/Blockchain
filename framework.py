519cba4059db27277779d401c77cb36a3eab0616
Sina Qahramani <sinaqahramani@gmail.com>
2020-08-03 23:03:33 +0430

    new_trx created

--- framework.py
+++ framework.py
@@ -19,7 +19,19 @@ def mine():
 
 @app.route('/trx/new', methods=['POST'])
 def new_trx():
-    return "new transaction"
+    values = request.get_json()
+    # check that te required fields are in the POSTed data:
+    required = ['sender', 'recepient', 'amount']
+    if not all (k in values for k in required):
+        return 'Missing Values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_trx(values['sender'], values['recepient'], values['amount'])
+    res = {
+        'message': f'Transaction will be added to Block {index}'
+    }
+
+    return jsonify(res), 201
 
 
 @app.route('/chain', methods=['GET'])
